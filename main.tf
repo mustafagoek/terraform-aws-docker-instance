5cd9cef864fcf38cedcfd5c5de57894c61414484
mustafagoek <gokmus@gmail.com>
2023-03-27 09:16:47 +0000

    should define your key file

---
+++ main.tf
@@ -0,0 +1,68 @@
+data "aws_ami" "amazon-linux-2" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-kernel-5.10*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+}
+
+data "template_file" "userdata" {
+  template = file("${abspath(path.module)}/userdata.sh")
+  vars = {
+    server-name = var.server-name
+  }
+}
+
+resource "aws_instance" "tfmyec2" {
+  ami           = data.aws_ami.amazon-linux-2.id
+  instance_type = var.instance_type
+  key_name      = var.key_name
+  count         = var.num_of_instance
+  #security_groups = [ "Docker-Instanceerraform-sec-grp" ]
+  vpc_security_group_ids = [aws_security_group.tf-sec-gr.id]
+  user_data              = data.template_file.userdata.rendered
+
+  tags = {
+    Name = var.tag
+  }
+}
+
+resource "aws_security_group" "tf-sec-gr" {
+  name = "${var.tag}-terraform-sec-grp"
+
+  dynamic "ingress" {
+    for_each = var.docker-instance-ports
+    iterator = port
+    content {
+      from_port   = port.value
+      to_port     = port.value
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
